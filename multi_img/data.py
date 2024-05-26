995eaee0613fd5ebce9c7b86d8ede1e34be80a26
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2024-05-26 13:39:57 +0200

    changing to efficiency

--- multi_img/data.py
+++ multi_img/data.py
@@ -69,11 +69,8 @@ CLUSTERED_PATH = os.path.join(DATA_DIR, 'clustered_images_with6classes.csv')
 def Efficiency(efficiency):
 
     efficiency = efficiency[efficiency["Advertisement Version Id"] == 1]
-    print(efficiency.columns)
-    efficiency['BuildingID'] = efficiency['Property Reference Id'].apply(lambda x: x.split('.')[1])
-    print(efficiency.columns)
-    efficiency['ApartmentID'] = efficiency['Property Reference Id'].apply(lambda x: x.split('.')[2])
-    efficiency = efficiency.groupby(['PropertyID', 'BuildingID', "ApartmentID", "PropertyFE"]).size().reset_index(name='counts').drop(columns="counts") 
+    efficiency = efficiency.groupby(["Property Reference Id", "PropertyFE"]).size().reset_index(name='counts').drop(columns="counts") 
+    print('The efficiency: ', efficiency.shape)
 
     return efficiency
 
