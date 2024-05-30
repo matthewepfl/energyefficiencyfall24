4052c928df09dd48f82aa23f24576eb037d49f47
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2024-05-30 14:56:13 +0200

    adding 4 other images

--- multi_img/data.py
+++ multi_img/data.py
@@ -183,7 +183,7 @@ def split(labels, val_size=0.1, test_size=0.15, seed=42):
     '''
     paths = [LABELS_TRAIN_PATH, LABELS_VAL_PATH, LABELS_TEST_PATH]
     
-    if all([os.path.exists(path) for path in paths]):
+    if False: # all([os.path.exists(path) for path in paths])
         print('Splitting:\tLoading pre-processed train, val, and test sets.')
         labels_train = pd.read_csv(LABELS_TRAIN_PATH)
         labels_val = pd.read_csv(LABELS_VAL_PATH)
@@ -418,7 +418,6 @@ def load_data(image_data, vision=None):
     '''
     print(f'LOADING DATA (vision: {vision})')
     print(f'Loaded image data:\tTrain: {len(image_data["train"])}\tValidation: {len(image_data["val"])}\tTest: {len(image_data["test"])} samples.')
-    print("the data is: ", image_data['train'])
     train_data = MultimodalDataset(vision, image_data['train'], augment=True)
     val_data = MultimodalDataset(vision, image_data['val'], augment=False)
     test_data = MultimodalDataset(vision, image_data['test'], augment=False)
