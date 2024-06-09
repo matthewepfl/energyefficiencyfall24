0896267e2019d5cd733cb989263bfda116bcf22b
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2024-06-09 13:41:14 +0200

    evaluation only

--- multi_img/data.py
+++ multi_img/data.py
@@ -211,19 +211,9 @@ def split(labels, val_size=0.1, test_size=0.15, seed=42):
         print("the labels_train: ", labels_train.shape, labels_train[:10])
 
         # Save the train, val, and test sets
-        np.save(LABELS_TRAIN_PATH, labels_train)
-        np.save(LABELS_VAL_PATH, labels_val)
-        np.save(LABELS_TEST_PATH, labels_test)
-
-        saved_train = np.load(LABELS_TRAIN_PATH, allow_pickle=True)
-        saved_val = np.load(LABELS_VAL_PATH, allow_pickle=True)
-        saved_test = np.load(LABELS_TEST_PATH, allow_pickle=True)
-
-        # compare the saved and the original
-        print('The comparison between the original and the saved')
-        print('The train: ', labels_train.shape, saved_train.shape)
-        print('The train: ', labels_train, saved_train)
-        
+        pd.to_csv(LABELS_TRAIN_PATH, labels_train)
+        pd.to_csv(LABELS_VAL_PATH, labels_val)
+        pd.to_csv(LABELS_TEST_PATH, labels_test)        
 
         # Check proportions of total, train, val, and test sets
         total_len = len(labels_train) + len(labels_val) + len(labels_test)
@@ -362,6 +352,12 @@ class MultimodalDataset(Dataset):
             inputs['x_5'] = image_5
 
         return inputs
+    
+    def get_labels(self):
+        labels = []
+        for idx in range(len(self)):
+            labels.append(self.__getitem__(idx)['labels'])
+        return torch.stack(labels)
 
     def collate_fn(self, batch):
         inputs = {}
