086ff3d10f28e459bf53e9e9f7546b95076f62ed
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2024-07-03 13:00:26 +0200

    small fix

--- multi_img/train.py
+++ multi_img/train.py
@@ -255,7 +255,7 @@ if __name__ == '__main__':
     parser.add_argument('--hidden_dims', default=[256, 512], help='Hidden dimensions for the MLP.')
     parser.add_argument('--dropout_prob', type=float, default=0.0)
     parser.add_argument('--batch_norm', action='store_true', default=False)
-    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--lr', type=list, default=[0.001])
     parser.add_argument('--weight_decay', type=float, default=0.0)
     parser.add_argument('--num_epochs', type=int, default=20)
     parser.add_argument('--seed', type=int, default=0)
